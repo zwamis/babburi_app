f669732d324e51e287536a9a2ef3bed2d88efb62
swamy <babburiswamy@gmail.com>
2018-04-09 10:47:55 +0530

    Finish static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home
+    #puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  end
+
+  def help
+  end
+  def about
+    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+  end
+end
